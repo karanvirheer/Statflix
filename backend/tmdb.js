ae0087f0f1a35b9dc3103de9a553cea741e867c1
Karanvir Heer <karanvirheer77@gmail.com>
2025-04-14 19:46:53 -0400

    Added Docstring

--- backend/tmdb.js
+++ backend/tmdb.js
@@ -8,6 +8,19 @@ const tmdb = axios.create({
   },
 });
 
+/*
+ * ==============================
+ *        TV SHOW API
+ * ==============================
+ */
+
+/**
+ * Searches for a TV show
+ *
+ * @param {string} query - Title of TV show
+ * @returns {dict} All matching TV shows with that title
+ * Reference: https://developer.themoviedb.org/reference/search-tv
+ */
 const searchTVShow = async (query) => {
   try {
     const res = await tmdb.get("/search/tv", {
@@ -16,10 +29,40 @@ const searchTVShow = async (query) => {
     return res.data;
   } catch (err) {
     console.error("Error fetching TV show:", err.response?.data || err.message);
-    throw err;
+    return false;
   }
 };
 
+/**
+ * Gets the TV details for a TV show
+ *
+ * @param {string} series_id - TMDb ID for a TV  show
+ * @returns {dict} Details about the TV show
+ * Reference: https://developer.themoviedb.org/reference/tv-series-details
+ */
+const getTVDetails = async (series_id) => {
+  try {
+    const res = await tmdb.get("/tv/" + String(series_id));
+    return res.data;
+  } catch (err) {
+    console.error("Error fetching TV show:", err.response?.data || err.message);
+    return false;
+  }
+};
+
+/*
+ * ==============================
+ *        MOVIE API
+ * ==============================
+ */
+
+/**
+ * Searches for a movie
+ *
+ * @param {string} query - Title of movie
+ * @returns {dict} All matching movies with that title
+ * Reference: https://developer.themoviedb.org/reference/search-movie
+ */
 const searchMovie = async (query) => {
   try {
     const res = await tmdb.get("/search/movie", {
@@ -28,23 +71,30 @@ const searchMovie = async (query) => {
     return res.data;
   } catch (err) {
     console.error("Error fetching Movie:", err.response?.data || err.message);
-    throw err;
+    return false;
   }
 };
 
-// https://developer.themoviedb.org/reference/tv-series-details
-const tvDetails = async (series_id) => {
+/**
+ * Gets the movie details for a movie
+ *
+ * @param {string} series_id - TMDb ID for a movie
+ * @returns {dict} Details about the movie
+ * Reference: https://developer.themoviedb.org/reference/movie-details
+ */
+const getMovieDetails = async (movie_id) => {
   try {
-    const res = await tmdb.get("/tv/" + String(series_id));
+    const res = await tmdb.get("/movie/" + String(movie_id));
     return res.data;
   } catch (err) {
-    console.error("Error fetching TV show:", err.response?.data || err.message);
-    throw err;
+    console.error("Error fetching movie:", err.response?.data || err.message);
+    return false;
   }
 };
 
 module.exports = {
   searchTVShow,
   searchMovie,
-  tvDetails,
+  getTVDetails,
+  getMovieDetails,
 };
